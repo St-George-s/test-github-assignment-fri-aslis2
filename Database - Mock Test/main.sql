ad147b5cc340e72d603350c730fff3b789f281b4
Jack <J.Walker-35@sms.ed.ac.uk>
2024-12-09 23:32:33 +0000

    Added Class Test

--- Database - Mock Test/main.sql
+++ Database - Mock Test/main.sql
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 SELECT name, COUNT(bookingID) as [Total Bookings]
 FROM Shop, Event, Booking 
 Where Shop.shopID == Event.shopID and Event.eventID == Booking.eventID
 Group by Shop.shopID
 Order by name ASC;
+=======
+>>>>>>> a8ee91c (Added Class Test)
