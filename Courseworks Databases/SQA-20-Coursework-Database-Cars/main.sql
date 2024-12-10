74997c307e0aa0638f027ab32287eee84a3fc49d
aslis2 <alt003@stge.org.uk>
2024-12-10 09:21:45 +0000

    mock cw

--- Courseworks Databases/SQA-20-Coursework-Database-Cars/main.sql
+++ Courseworks Databases/SQA-20-Coursework-Database-Cars/main.sql
@@ -1,1 +1,6 @@
-DESCRIBE ALL TABLES;
+SELECT forename, surname, AVG(cost) AS [Average Job Cost]
+FROM Customer, Car, Job
+WHERE Customer.customerID = Car.customerID AND
+ Car.regNo = Job.regNo
+GROUP BY cost, forename, surname
+ORDER BY AVG(cost) DESC;
--- Database - Mock Test/main.sql
+++ Database - Mock Test/main.sql
@@ -1,1 +1,5 @@
-DESCRIBE ALL TABLES;
+SELECT name, COUNT(bookingID) as [Total Bookings]
+FROM Shop, Event, Booking 
+Where Shop.shopID == Event.shopID and Event.eventID == Booking.eventID
+Group by Shop.shopID
+Order by name ASC;
