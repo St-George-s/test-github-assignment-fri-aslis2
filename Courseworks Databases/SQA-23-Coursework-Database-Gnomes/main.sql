c8ca2534c5f1902ecd13c1adc129ecc65868c512
aslis2 <alt003@stge.org.uk>
2025-02-12 23:36:24 +0000

    23 and 20 databases

--- Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
+++ Courseworks Databases/SQA-23-Coursework-Database-Gnomes/main.sql
@@ -1,3 +1,20 @@
+
+SELECT gnomeName, SUM(quantity) as [Total gnomes sold]
+FROM Gnome, GnomePurchase
+WHERE Gnome.gnomeID = GnomePurchase.gnomeID and description like "%solar%"
+GROUP BY gnomeName
+ORDER BY SUM(quantity) DESC;
+
+
+SELECT emailAddress, Orders.orderID, quantity 
+FROM Customer, Orders, GnomePurchase, Gnome 
+WHERE Customer.customerID=Orders.customerID  
+    AND Orders.orderID=GnomePurchase.orderID 
+    AND GnomePurchase.gnomeID=Gnome.gnomeID 
+    AND quantity >= 3 
+    AND unitPrice = ( SELECT MAX(unitPrice) FROM Gnome ); 
+
+
 SELECT forename, surname, SUM(quantity * unitPrice * 1.2) AS [Total to Pay £]
 FROM Customer, Gnome, GnomePurchase, Orders
 WHERE Orders.orderID="ord0024" 
