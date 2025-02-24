0a19cf9e0288b3a2de072d851782a95dfd61e944
aslis2 <alt003@stge.org.uk>
2025-02-24 12:05:15 +0000

    software 2025

--- 2025 coursework/main.py
+++ 2025 coursework/main.py
@@ -10,7 +10,7 @@ class Orders:
         self.rating = rating
 
 #Read from file into array of records 
-def read_data():
+def readData():
     file ="2025 coursework/orders.txt"
     with open(file,'r') as file:
         reader = csv.reader(file)
@@ -22,30 +22,32 @@ def read_data():
 
     return orders
 
+
 #Find the position of the customer who gave the first 5-star rating in a given month.
-def find_Position(orders):
+def findPosition(orders):
     position = -1
     index = 0 
+    #Ask user to enter month to search for
     month = input("Enter month ")
     while position == -1 and index < len(orders):
-        if month == (orders[index].date[3:6]) and orders[index].rating == 5:
-            index = position
+        #Use string manipulation to get the portion of the string thats the month and check if the rating is 5
+        if month == (orders[index].date[3:6]) and str(orders[index].rating) == "5":
+            position = index
         index = index + 1 
-        break
-    print(orders[index].rating)
-    print(orders[index].date[3:6])
-    print(position)
     return position
 
 
+#Write details of the winning customer, or ‘no winner’ message, to a text file.
 def winningCustomers(orders, position):
     with open('winningCustomer.txt', 'w', newline= '' ) as file:
         if position >= 0:
-                file.write(orders.orderNum + "," + orders.email + "," + orders.cost + "\n" )
+                file.write(orders[position].orderNum + "," + orders[position].email + "," + orders[position].cost + "\n" )
         else:
             file.write("No winner" + "\n")
-        
-def totalOrders_Delivered_Collected(orders):
+
+
+#Display the total number of orders delivered and the total number of orders collected.       
+def countOption(orders):
     count = 0
     for i in range(0, len(orders)):
         if "Delivery" == orders[i].option :
@@ -61,10 +63,10 @@ def totalOrders_Delivered_Collected(orders):
 
 
 #main
-orders = read_data() 
-position = find_Position(orders)
+orders = readData() 
+position = findPosition(orders)
 winningCustomers(orders, position)
-totalOrders_Delivered_Collected(orders)
+countOption(orders)
 
 
 
