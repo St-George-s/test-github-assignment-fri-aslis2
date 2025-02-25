ed8d20b2172c031f2d5f6baa42db46bb757e07fe
aslis2 <alt003@stge.org.uk>
2025-02-25 09:43:52 +0000

    software 2025 done

--- 2025 coursework/main.py
+++ 2025 coursework/main.py
@@ -9,7 +9,8 @@ class Orders:
         self.cost = cost
         self.rating = rating
 
-#Read from file into array of records 
+
+#Read data from the ‘orders.txt’ file and store into an array of records
 def readData():
     file ="2025 coursework/orders.txt"
     with open(file,'r') as file:
@@ -23,17 +24,18 @@ def readData():
     return orders
 
 
-#Find the position of the customer who gave the first 5-star rating in a given month.
+#Find and return the position of the customer who gave the first 5-star rating in a given month.
 def findPosition(orders):
     position = -1
     index = 0 
     #Ask user to enter month to search for
-    month = input("Enter month ")
+    month = input("Enter the first three letters of the month to search: ")
     while position == -1 and index < len(orders):
         #Use string manipulation to get the portion of the string thats the month and check if the rating is 5
-        if month == (orders[index].date[3:6]) and str(orders[index].rating) == "5":
+        if month == (orders[index].date[3:6]) and int(orders[index].rating) == 5 :
             position = index
         index = index + 1 
+
     return position
 
 
@@ -41,32 +43,43 @@ def findPosition(orders):
 def winningCustomers(orders, position):
     with open('winningCustomer.txt', 'w', newline= '' ) as file:
         if position >= 0:
+                #Write winning order number, email and cost to ‘winningCustomer.txt
                 file.write(orders[position].orderNum + "," + orders[position].email + "," + orders[position].cost + "\n" )
         else:
+            #Write ‘No winner’ to ‘winningCustomer.txt’
             file.write("No winner" + "\n")
 
 
-#Display the total number of orders delivered and the total number of orders collected.       
+#Find and return the total number of orders delivered and the total number of orders collected      
 def countOption(orders):
-    count = 0
+    totalDelivery = 0
+    totalCollected = 0 
+    #Loop for length of the record
     for i in range(0, len(orders)):
         if "Delivery" == orders[i].option :
-            count = count +1 
-    print("Total number of orders delivered to date: " + str(count))
+            totalDelivery = totalDelivery +1 
+        else: 
+            totalCollected = totalCollected + 1 
 
-    found = 0
-    for i in range(0, len(orders)):
-        if "Collection" == orders[i].option :
-            found = found +1 
-    print("Total number of orders collected to date: " + str(found))
+    return totalDelivery, totalCollected
 
 
+#Display the total number of orders delivered and the total number of orders collected. 
+def displayOption(orders):
+    totalDelivery, totalCollected = countOption(orders)
 
-#main
+    #Output the total number of orders delivered
+    print("Total number of orders delivered to date: " + str(totalDelivery))
+    #Output the total number of orders collected
+    print("Total number of orders collected to date: " + str(totalCollected))
+
+
+
+#Main
 orders = readData() 
 position = findPosition(orders)
 winningCustomers(orders, position)
-countOption(orders)
+displayOption(orders)
 
 
 
