f3f3d535626a1110ac81b66fad6d072b07139e6f
aslis2 <alt003@stge.org.uk>
2025-02-28 09:19:41 +0000

    2025 coursework

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -14,15 +14,10 @@ ORDER BY  SUM(valuation) DESC;
 SELECT comicTitle, issue, publisherName, valuation * 2 AS [Double Price]
 FROM Comic, Publisher, Series, CharacterInfo, ComicCharacter
 WHERE Series.seriesName = "The OK Seven"
-
 AND characterName == "Starlordly"
-
 AND Comic.publisherID == Publisher.publisherID
-
 AND Comic.seriesID == Series.seriesID
-
 AND Comic.comicID == ComicCharacter.comicID
-
 AND CharacterInfo.characterID == ComicCharacter.characterID;
 
 
